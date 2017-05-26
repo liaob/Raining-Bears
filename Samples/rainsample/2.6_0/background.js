97178fae8b0d9a315ea6e6bea393c4f7ac096ba2
Bryan Liao <bryan.liao@stonybrook.edu>
2017-05-26 12:09:43 -0400

    added in rilakkuma

--- Samples/rainsample/2.6_0/background.js
+++
@@ -1,4 +0,0 @@
-if ((localStorage["onInstall"] != "false") && (localStorage["onInstall"] != false)){
-	chrome.tabs.create({url: "http://rainonme.github.io/rain-on-me/", selected:true})
-	localStorage["onInstall"] = false;
-}
